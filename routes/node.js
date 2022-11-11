367d6dc9a4f7397f200a34aabb5c626bb2ae3648
Tharu <u7145170@anu.edu.au>
2022-11-11 21:33:05 +1100

    add routes

--- routes/node.js
+++ routes/node.js
@@ -1,9 +1,35 @@
 var express = require('express');
+const router = require('../router');
 var router = express.Router();
-
+//PREFIX = /node
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+//CREATE
+//Create node with label and relationships
+router.post('/:label')
+
+//PUT
+//Update a node's properties
+router.put('/properties/:id')
+
+//Update a node's label
+router.put('/labels/:id/:label')
+
+//DELETE
+//Delete a node with its relationships
+router.delete('/:id')
+//Delete a node's properties
+router.delete('/properties/:id')
+
+//READ
+//Read a certain node
+router.get('/:id')
+//Read a node label
+router.get('/labels/:id')
+//Read a node's properties
+router.get('/properties/:id')
--- routes/relationship.js
+++ routes/relationship.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
-
+//PREFIX = /relationship
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 module.exports = router;
+
+//CREATE
+router.post('/')
+
+//UPDATE
+//Update the properties of a relationship
+router.post('/properties/:id')
+//Update existing relationship with certain type
+router.post('/properties/type/:id/:type')
+
+//DELETE 
+//Delete a node
+router.delete('/:id')
+//Delete properties property
+router.delete('/properties')
+
+//GET
+//Get a relationship
+router.get('/:id')
+//Get a relationship type
+router.get('type/:id')
+//Get the properties of a relationship
+router.get('properties/:id')
+
